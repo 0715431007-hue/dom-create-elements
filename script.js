d5f0e134c1ad3263fef2f04438f5969d3607b373
0715431007-hue <0715431007@s.tusd1.org>
2026-03-26 16:26:58 +0000

    commit

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("script started");
+
+function start() {
+    console.log("click");
+
+    // create h1 heading
+    let heading = document.createElement("h1");
+
+    // set up text and styles
+    heading.innerText = "Attendance";
+    heading.style.color = "purple";
+    heading.style.textDecoration = "underline";
+
+    // add the h1 to the page
+    document.body.appendChild(heading);
+
+    // prompting user for name
+    let userName = prompt("What is your name?");
+    console.log(userName);
+
+    // addName function to add name
+    addName(userName);
+
+}
+
+
+function addName(newName) {
+    console.log(newName);
+    //creates h2 element
+    let h2 = document.createElement("h2");
+
+    //sets h2 as newName
+    h2.innerText = newName;
+
+    //prints h2 out
+    document.body.appendChild(h2);
+}
